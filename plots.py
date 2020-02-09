3934c4cdc822b5d0cb9fc8cb76bc29a647d08229
Steffgroe <landmacht_steff@hotmail.com>
2020-02-09 22:41:07 +0100

    plots and stuff

---
+++ plots.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+accuracies = np.genfromtxt('accuracies.csv',delimiter=',')
+val_accuracies = np.genfromtxt('val_accuracies.csv',delimiter=',')
+losses = np.genfromtxt('losses.csv',delimiter=',')
+val_losses = np.genfromtxt("val_losses.csv",delimiter =',')
+epochs_range = 15
+optimizers = [ 'adam','sgd','rmsprop','adamax','adadelta']
+fig = plt.figure(figsize=(8, 8))
+
+plt.subplot(2, 2, 1)
+for idx in range (0,5):
+	temp = (accuracies[idx+1])
+	temp = temp[1:]
+	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp,label='Training accuracy with ' + str(optimizers[idx]))
+
+plt.title('Training Accuracy')
+
+plt.subplot(2, 2, 2)
+for idx in range (0,5):
+	temp = (losses[idx+1])
+	temp = temp[1:]
+	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp,label='Training loss with ' + str(optimizers[idx]))
+plt.title('Training loss')
+plt.subplot(2, 2, 3)
+for idx in range (0,5):
+	temp = (val_accuracies[idx+1])
+	temp = temp[1:]
+	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp, label='Validation accuracy with ' + str(optimizers[idx]))
+	
+
+plt.title('Validation Accuracy')
+
+plt.subplot(2, 2, 4)
+for idx in range (0,5):
+	temp = (val_losses[idx+1])
+	temp = temp[1:]
+	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp, label='Validation Loss with dropout '+ str(optimizers[idx]))
+
+plt.title('Validation Loss')
+fig.legend(optimizers,loc='lower center',ncols=5)
+plt.show()
