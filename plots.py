1b22429777a3f8e97d49dde579609f2b3e69e3bb
Steffgroe <Steffgroe@users.noreply.github.com>
2020-02-10 10:25:58 +0100

    Update plots.py

--- plots.py
+++ plots.py
@@ -10,20 +10,24 @@ val_losses = np.genfromtxt("val_losses.csv",delimiter =',')
 epochs_range = 15
 optimizers = [ 'adam','sgd','rmsprop','adamax','adadelta']
 fig = plt.figure(figsize=(8, 8))
+plt.style.use('ggplot')
 
 plt.subplot(2, 2, 1)
 for idx in range (0,5):
 	temp = (accuracies[idx+1])
 	temp = temp[1:]
 	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp,label='Training accuracy with ' + str(optimizers[idx]))
-
+	# plt.errorbar([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp, yerr=np.std(temp))
 plt.title('Training Accuracy')
 
 plt.subplot(2, 2, 2)
+
 for idx in range (0,5):
 	temp = (losses[idx+1])
 	temp = temp[1:]
+	plt.ylim(top=1,bottom=0.5)
 	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp,label='Training loss with ' + str(optimizers[idx]))
+	
 plt.title('Training loss')
 plt.subplot(2, 2, 3)
 for idx in range (0,5):
@@ -38,8 +42,9 @@ plt.subplot(2, 2, 4)
 for idx in range (0,5):
 	temp = (val_losses[idx+1])
 	temp = temp[1:]
+
 	plt.plot([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],temp, label='Validation Loss with dropout '+ str(optimizers[idx]))
 
 plt.title('Validation Loss')
-fig.legend(optimizers,loc='lower center',ncols=5)
+fig.legend(optimizers,loc='lower center',ncol=5)
 plt.show()
